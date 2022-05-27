b78369dc6a00b4b25e25c99bfb09439b9c1ddba3
Juan de Dios Quiroz <quirozrodriguezj@gmail.com>
2022-05-26 23:05:30 -0500

    Added comments

--- src/test/java/com/CreateEventTest.java
+++ src/test/java/com/CreateEventTest.java
@@ -14,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 
 @RunWith(JUnit4.class)
 public class CreateEventTest {
+//    ola
 //    private static WebDriver driver = null;
 //    private Map<String, Object> vars;
 //    JavascriptExecutor js;
